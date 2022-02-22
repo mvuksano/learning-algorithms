104ea1b746ac77b1dbfc21b63909577cee4df0a3
Marko Vuksanovic <markovuksanovic@gmail.com>
2022-02-21 17:27:05 -0800

    Store benchmark results to file.

--- bench/main.cc
+++ bench/main.cc
@@ -1,8 +1,10 @@
 #include <algorithm>
 #include <cmath>
 #include <cstdint>
+#include <fstream>
 #include <vector>
 
+#include <gflags/gflags.h>
 #include <glog/logging.h>
 
 #define TIME_IT(hi, lo)                                                        \
@@ -10,7 +12,9 @@
       "rdtsc\n\t"                                                              \
       : "=d"(hi), "=a"(lo))
 
-void __attribute__((optimize("O0"))) fn(uint32_t repeat, uint64_t size) {
+DEFINE_string(output, "", "Filename where to store output of this run.");
+
+double __attribute__((optimize("O0"))) fn(uint32_t repeat, uint64_t size) {
   uint32_t start_time_hi = 0, start_time_lo = 0, end_time_hi = 0,
            end_time_lo = 0;
   uint64_t start = 0, end = 0;
@@ -26,13 +30,31 @@ void __attribute__((optimize("O0"))) fn(uint32_t repeat, uint64_t size) {
   start = ((uint64_t)start_time_hi << 32) | start_time_lo;
   end = ((uint64_t)end_time_hi << 32) | end_time_lo;
 
-  LOG(INFO) << "Cycles / op = " << ((end - start) / ((double)repeat * size));
+  auto cycles_per_op = ((end - start) / ((double)repeat * size));
+  LOG(INFO) << "Cycles / op = " << cycles_per_op;
+  return cycles_per_op;
+}
+
+void store_results_to_file(
+    const std::string filename,
+    const std::vector<std::pair<uint, double>> &results) noexcept {
+  std::ofstream file(filename);
+  std::for_each(results.cbegin(), results.cend(),
+                [&](auto e) { file << e.first << "\t" << e.second << "\n"; });
 }
 
 int main(int argc, char **argv) {
-  google::InitGoogleLogging(argv[0]);
-
   std::vector<std::pair<uint, uint64_t>> buckets;
+  std::vector<std::pair<uint, double>> results;
+
+  google::InitGoogleLogging(argv[0]);
+  gflags::ParseCommandLineFlags(&argc, &argv, true);
+
+  if (FLAGS_output != "") {
+    LOG(INFO) << "Result of this benchmark will be stored in " << FLAGS_output
+              << " file.";
+  }
+
   int upper_bound = 30;
   for (int i = 2; i < upper_bound; i++) {
     buckets.push_back({i, std::pow(2, i)});
@@ -45,8 +67,13 @@ int main(int argc, char **argv) {
               << " bytes)";
     uint32_t repeat = run_number_of_times / el.second;
     LOG(INFO) << "Repeating measurement " << repeat << " times.";
-    fn(repeat, el.second);
+    results.push_back({el.first, fn(repeat, el.second)});
   });
+
+  if (FLAGS_output != "") {
+    LOG(INFO) << "Storing results into " << FLAGS_output;
+    store_results_to_file(FLAGS_output, results);
+  }
 
   return 0;
 }
